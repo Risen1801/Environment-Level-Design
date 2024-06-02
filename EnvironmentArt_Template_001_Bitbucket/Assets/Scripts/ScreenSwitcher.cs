fdd9d1b1451a5b981493bdfbe0eaf50aec4676eb
MafackerJones <lust20035@hs-ansbach.de>
2024-06-02 22:19:40 +0200

    Added last Scene
    - Script for SceneChange
    - Last Scene Layout

---
+++ EnvironmentArt_Template_001_Bitbucket/Assets/Scripts/ScreenSwitcher.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher : MonoBehaviour
+{
+    // Name der Szene, zu der gewechselt werden soll
+    public string sceneName;
+
+    // Diese Methode wird aufgerufen, wenn ein anderes Collider-Objekt in den Trigger-Box-Collider eintritt
+    private void OnTriggerEnter(Collider other)
+    {
+        // Überprüfen, ob das Objekt, das in den Collider eintritt, der Spieler ist
+        if (other.CompareTag("Player"))
+        {
+            // Szene wechseln
+            SceneManager.LoadScene(sceneName);
+        }
+    }
+}
