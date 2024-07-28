a5fe105a28e1843505792498a64e4356d6f4db95
MafackerJones <lust20035@hs-ansbach.de>
2024-07-28 20:19:46 +0200

    Bugfix on PickUpAndExamine Skript
    Due to this Script not being made by me and given to us by our Professor, i modified it to use the StarterAssetInput-System.
    After A while we stumbled upon a bug, where the player was able to drop the item while examining, locking him into a state of not being able to move and being forced to restart the game.
    With the help of ChatGPT i was able to fix this issue.

--- EnvironmentArt_Template_001_Bitbucket/Assets/Scripts/PickUpAndExamine.cs
+++ EnvironmentArt_Template_001_Bitbucket/Assets/Scripts/PickUpAndExamine.cs
@@ -29,6 +29,7 @@ public class PickUpAndExamine : MonoBehaviour
     private StarterAssetsInputs _starterAssetsInputs;
 
     // Use this for initialization
+    /*
     void Start()
     {
         SetBaseFOV(GetComponent<Camera>().fieldOfView);
@@ -166,6 +167,115 @@ public class PickUpAndExamine : MonoBehaviour
 
         UpdateZoom();
 
+    }
+
+    private void UpdateZoom()
+    {
+        GetComponent<Camera>().fieldOfView = Mathf.Lerp(GetComponent<Camera>().fieldOfView, targetFOV, zoomSpeed * Time.deltaTime);
+    }
+
+    public void SetBaseFOV(float fov)
+    {
+        baseFOV = fov;
+    }
+
+    */
+    void Start()
+    {
+        SetBaseFOV(GetComponent<Camera>().fieldOfView);
+        _player = GameObject.FindWithTag("Player");
+        _starterAssetsInputs = _player.GetComponent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        // Pick up or drop object
+        if (Input.GetButtonDown("Fire1") && !examiningObject)
+        {
+            if (!objectIsPickedUp)
+            {
+                Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5F, 0.5F, 0));
+                RaycastHit hit;
+                if (Physics.Raycast(ray, out hit))
+                {
+                    if (hit.transform.gameObject.GetComponent<Rigidbody>() != null)
+                    {
+                        if (hit.transform.gameObject.GetComponent<PickUpObject>())
+                        {
+                            hitObject = hit.transform.gameObject;
+                            myPickUpObjectScript = hitObject.GetComponent<PickUpObject>();
+                            hitObject.transform.parent = handPosition.transform;
+                            hitObject.transform.position = handPosition.transform.position;
+                            hitObject.GetComponent<Rigidbody>().isKinematic = true;
+                            objectIsPickedUp = true;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                DropObject();
+            }
+        }
+
+        // Rotate object while examining
+        if (examiningObject)
+        {
+            if (myPickUpObjectScript.rotateHorizontal)
+            {
+                hitObject.transform.Rotate(new Vector3(Input.GetAxis("Mouse Y"), 0, 0));
+            }
+            if (myPickUpObjectScript.rotateVertical)
+            {
+                hitObject.transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0));
+            }
+        }
+
+        // Start examining
+        if (Input.GetButtonDown("Fire2") && objectIsPickedUp && !examiningObject)
+        {
+            StartExamining();
+        }
+
+        // End examining
+        if (Input.GetButtonUp("Fire2") && examiningObject)
+        {
+            StopExamining();
+        }
+
+        // Zoom
+        targetFOV = (Input.GetButton("Fire2") && !objectIsPickedUp) ? zoomFOV : baseFOV;
+        UpdateZoom();
+    }
+
+    private void DropObject()
+    {
+        hitObject.transform.parent = null;
+        hitObject.GetComponent<Rigidbody>().isKinematic = false;
+        hitObject.GetComponent<Rigidbody>().AddForce(transform.forward * thrust);
+        hitObject = null;
+        objectIsPickedUp = false;
+    }
+
+    private void StartExamining()
+    {
+        hitObject.transform.position = examinePosition.transform.position;
+        if (myPickUpObjectScript.adjustObject)
+        {
+            hitObject.transform.eulerAngles = gameObject.transform.eulerAngles;
+        }
+        examiningObject = true;
+        myCharacterController.enabled = false;
+        _starterAssetsInputs.cursorInputForLook = false;
+        _starterAssetsInputs.LookInput(Vector2.zero);
+    }
+
+    private void StopExamining()
+    {
+        hitObject.transform.position = handPosition.transform.position;
+        examiningObject = false;
+        myCharacterController.enabled = true;
+        _starterAssetsInputs.cursorInputForLook = true;
     }
 
     private void UpdateZoom()
